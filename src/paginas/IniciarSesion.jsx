f06ca8ad52cef1b1305bfc62cb7d7b920ce65710
unknown <Jernimo>
2025-07-27 18:45:11 -0300

    se agrega un formulario de la librería react bootstrap

--- src/paginas/IniciarSesion.jsx
+++ src/paginas/IniciarSesion.jsx
@@ -4,7 +4,9 @@ import { cambiarTituloPagina } from "../funciones_auxiliares/cambiarTituloPagina
 const IniciarSesion = () => {
   cambiarTituloPagina("Iniciar Sesion")
   return (
-    <div>IniciarSesion</div>
+    <>
+    
+    </>
   )
 }
 
