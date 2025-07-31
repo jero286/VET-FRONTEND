510635c167dcf9e02e1347b26356fde80c51c605
unknown <Jernimo>
2025-07-31 20:36:59 -0300

    navbar adaptado para admin y usuario

--- src/componentes/navbar/NavbarC.jsx
+++ src/componentes/navbar/NavbarC.jsx
@@ -1,33 +1,85 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router';
-import "./navbar.css"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, useNavigate } from "react-router";
+import "./navbar.css";
+import { Button } from "react-bootstrap";
 
 const NavbarC = () => {
+  const navigate = useNavigate();
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const usuarioLogueadoRol = JSON.parse(sessionStorage.getItem("rol"));
+
+  const cerrarSesion = (ev) => {
+    ev.preventDefault();
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("rol");
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
   return (
     <>
-    <Navbar expand="lg" className="body-tertiary">
-      <Container>
-           <NavLink className={"nav-link"} to={"/"}>
-            <img src="/logo3.jpeg" alt="Logo" style={{width: '99px'}} />
-            </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <NavLink className={"nav-link"} to="/">Inicio</NavLink>
-            <NavLink className={"nav-link"} to="/contacto">Contacto</NavLink>
-            <NavLink className={"nav-link"} to="/acercaDeNosotros">Acerca de Nosotros</NavLink>
-          </Nav>
-          <Nav className="ms-auto">
-            <NavLink className={"nav-link"} to="/iniciarSesion">Iniciar Sesion</NavLink>
-            <NavLink className={"nav-link"} to="/registrarse">Registrarse</NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" className="body-tertiary">
+        <Container>
+          <NavLink
+            className={"nav-link"}
+            to={
+              token && usuarioLogueadoRol === "usuario"
+                ? "/user"
+                : token && usuarioLogueadoRol === "admin"
+                ? "/admin"
+                : "/"
+            }
+          >
+            <img src="/logo3.jpeg" alt="Logo" style={{ width: "99px" }} />
+          </NavLink>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {token && usuarioLogueadoRol === "usuario" ? (
+              <Nav className="ms-auto d-flex align-items-center gap-3">
+                <Button variant="primary" as={NavLink} to="/reservar-turno">
+                  Reservar turno
+                </Button>
+                <NavLink className="nav-link" to="#" onClick={cerrarSesion}>
+                  Cerrar Sesión
+                </NavLink>
+              </Nav>
+            ) : token && usuarioLogueadoRol === "admin" ? (
+              <Nav className="ms-auto">
+                <NavLink className="nav-link" to="#" onClick={cerrarSesion}>
+                  Cerrar Sesión
+                </NavLink>
+              </Nav>
+            ) : (
+              <>
+                <Nav className="ms-auto">
+                  <NavLink className="nav-link" to="/">
+                    Inicio
+                  </NavLink>
+                  <NavLink className="nav-link" to="/contacto">
+                    Contacto
+                  </NavLink>
+                  <NavLink className="nav-link" to="/acercaDeNosotros">
+                    Acerca de Nosotros
+                  </NavLink>
+                </Nav>
+                <Nav className="ms-auto">
+                  <NavLink className="nav-link" to="/iniciarSesion">
+                    Iniciar Sesión
+                  </NavLink>
+                  <NavLink className="nav-link" to="/registrarse">
+                    Registrarse
+                  </NavLink>
+                </Nav>
+              </>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default NavbarC
+export default NavbarC;
+
