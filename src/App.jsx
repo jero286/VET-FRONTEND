a247767c01ea207466ac37ee693af735516dc921
MarianoTorresMari <marianomisael06@gmail.com>
2025-08-30 15:14:37 -0300

    se arregla el problema del footer, asi quede abajo siempre

--- src/App.jsx
+++ src/App.jsx
@@ -31,70 +31,72 @@ import UsuarioMascotas from "./paginas/UsuarioMascotas";
 
 const App = () => {
   return (
-    <>
+    <div className="app">   {/* contenedor principal con flex */}
       <Router>
         <NavbarC />
-        <Routes>
-          <Route path="/" element={<PaginaPrincipal />} />
-          <Route path="/planes" element={<Planes />} />
-          <Route path="/planes/:nombrePlan" element={<DetallePlan />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/acercaDeNosotros" element={<AcercaDeNosotros />} />
-          <Route path="/iniciarSesion" element={<IniciarSesion />} />
-          <Route path="/registrarse" element={<Registrarse />} />
-          <Route path="*" element={<Error404 />} />
-          <Route path="/usuario" element={<PaginaUsuario />} />
-          <Route path="/usuario/reservarTurnos" element={<ReservarTurno />} />
-          <Route path="/usuario/turnos" element={<UsuarioTurnos />} />
-          <Route path="/usuario/carrito" element={<UsuarioCarrito />} />
-          <Route path="/pagoExitoso" element={<PagoExitoso />} />
-          <Route path="/pagoPendiente" element={<PagoPendiente />} />
-          <Route path="/pagoFallido" element={<PagoFallido />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/pacientes" element={<AdminPacientes />} />
-          <Route path="/admin/turnos" element={<AdminTurnos />} />
-          <Route path="/admin/productos" element={<AdminProductos />} />
-          <Route
-            path="/admin/usuarios/crearEditar"
-            element={<AdminEditarUsuarios />}
-          />
-          <Route
-            path="/admin/productos/crearEditar"
-            element={<AdminCrearEditarProductos />}
-          />
-          <Route
-            path="/admin/turnos/editarTurnos"
-            element={<AdminEditarTurnos />}
-          />
-          <Route
-            path="/emailRecuperarContrasenia"
-            element={<RecuperarContraseniaEmail />}
-          />
-          <Route
-            path="/recuperarContraseniaForm"
-            element={<NuevaContrasenia />}
-          />
-          <Route
-            path="/admin/usuarios/Editar"
-            element={<AdminEditarUsuarios />}
-          />
-          <Route
-            path="/admin/productos/crearEditar"
-            element={<AdminCrearEditarProductos />}
-          />
-          <Route
-            path="/admin/turnos/editarTurnos"
-            element={<AdminEditarTurnos />}
-          />
-          <Route path="/usuario/mascotas" element={<UsuarioMascotas />} />
-          <Route
-            path="/usuario/mascotas/crearEditarMascota"
-            element={<UsuarioCrearEditarMascota />}
-          />
-        </Routes>
+        <main>   {/* acá se renderizan las rutas */}
+          <Routes>
+            <Route path="/" element={<PaginaPrincipal />} />
+            <Route path="/planes" element={<Planes />} />
+            <Route path="/planes/:nombrePlan" element={<DetallePlan />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/acercaDeNosotros" element={<AcercaDeNosotros />} />
+            <Route path="/iniciarSesion" element={<IniciarSesion />} />
+            <Route path="/registrarse" element={<Registrarse />} />
+            <Route path="*" element={<Error404 />} />
+            <Route path="/usuario" element={<PaginaUsuario />} />
+            <Route path="/usuario/reservarTurnos" element={<ReservarTurno />} />
+            <Route path="/usuario/turnos" element={<UsuarioTurnos />} />
+            <Route path="/usuario/carrito" element={<UsuarioCarrito />} />
+            <Route path="/pagoExitoso" element={<PagoExitoso />} />
+            <Route path="/pagoPendiente" element={<PagoPendiente />} />
+            <Route path="/pagoFallido" element={<PagoFallido />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/pacientes" element={<AdminPacientes />} />
+            <Route path="/admin/turnos" element={<AdminTurnos />} />
+            <Route path="/admin/productos" element={<AdminProductos />} />
+            <Route
+              path="/admin/usuarios/crearEditar"
+              element={<AdminEditarUsuarios />}
+            />
+            <Route
+              path="/admin/productos/crearEditar"
+              element={<AdminCrearEditarProductos />}
+            />
+            <Route
+              path="/admin/turnos/editarTurnos"
+              element={<AdminEditarTurnos />}
+            />
+            <Route
+              path="/emailRecuperarContrasenia"
+              element={<RecuperarContraseniaEmail />}
+            />
+            <Route
+              path="/recuperarContraseniaForm"
+              element={<NuevaContrasenia />}
+            />
+            <Route
+              path="/admin/usuarios/Editar"
+              element={<AdminEditarUsuarios />}
+            />
+            <Route
+              path="/admin/productos/crearEditar"
+              element={<AdminCrearEditarProductos />}
+            />
+            <Route
+              path="/admin/turnos/editarTurnos"
+              element={<AdminEditarTurnos />}
+            />
+            <Route path="/usuario/mascotas" element={<UsuarioMascotas />} />
+            <Route
+              path="/usuario/mascotas/crearEditarMascota"
+              element={<UsuarioCrearEditarMascota />}
+            />
+          </Routes>
+        </main>
         <FooterC />
       </Router>
-    </>
+    </div>
   );
 };
 
